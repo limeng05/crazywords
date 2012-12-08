675256a60f7bb2b44dc9239b7ecfcd1b513fb888
Chhuang <huangcd.thu@gmail.com>
2012-12-08 13:44:54 +0800

    Add mongoDB to project

---
+++ WordPuzzle/DataSource.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MongoDB.Driver;
+
+namespace WordPuzzle
+{
+    public class DataSource
+    {
+        public static MongoClient client = new MongoClient("mongodb://crazywords.cloudapp.net");
+    }
+}
--- WordPuzzle/MapGenerator.cs
+++ WordPuzzle/MapGenerator.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace WordPuzzle
 {
-    public class Class1
+    public class MapGenerator
     {
     }
 }
